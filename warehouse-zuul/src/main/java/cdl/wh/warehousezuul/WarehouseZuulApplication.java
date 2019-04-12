6bf677fadcd9940092fef49eb7eb77bfd8889ec9
pratik.waghela <pratik.waghela@cdl-it.com>
2019-04-12 13:35:34 +0530

    working in postman but gatway only authorize request when fist request will be authorized form single api

--- warehouse-zuul/src/main/java/cdl/wh/warehousezuul/WarehouseZuulApplication.java
+++ warehouse-zuul/src/main/java/cdl/wh/warehousezuul/WarehouseZuulApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient 	// It acts as a eureka client
-@EnableZuulProxy	
+@EnableZuulProxy
 public class WarehouseZuulApplication {
 
 	public static void main(String[] args) {
