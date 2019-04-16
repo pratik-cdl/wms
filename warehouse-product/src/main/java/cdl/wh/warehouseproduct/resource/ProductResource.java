43e1948d768dca80a0ffc758dcce802d91236ea9
pratik.waghela <pratik.waghela@cdl-it.com>
2019-04-16 14:42:23 +0530

    Resource base authrization by role user and wms-manager for all product

--- warehouse-product/src/main/java/cdl/wh/warehouseproduct/resource/ProductResource.java
+++ warehouse-product/src/main/java/cdl/wh/warehouseproduct/resource/ProductResource.java
@@ -9,10 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,31 +38,29 @@ public class ProductResource {
 	@GetMapping("/all")
 	public ResponseEntity<?> getAllProduct(){
 		
-		//Authentication auth = KeycloakSecurityContext.getContext().getAuthentication();
-		
-		//System.out.println(">>" + auth.getAuthorities());
-	
 		List<Product> result = productService.getAllProduct();
 		
 		return new ResponseEntity<>(result,HttpStatus.OK);
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<?> getAllCustomerDetails(@PathVariable("id") Long id){
+	public ResponseEntity<?> getAllProductDetails(@PathVariable("id") Long id){
 		
-		//Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		//System.out.println(">>" + auth.getAuthorities());
-	
 		Product result = productService.getProduct(id);
 		
 		return new ResponseEntity<>(result,HttpStatus.OK);
 	}
 	
-	@GetMapping("/helloWorld")
-	public ResponseEntity<?> demo(Principal p){
-	
+	@PostMapping("/")
+	public ResponseEntity<?> createProduct(@RequestBody Product p){
+		p.setName("createProduct");
 		System.out.println("principal :: "+p);
-		return new ResponseEntity<>("helloWorld",HttpStatus.OK);
+		return new ResponseEntity<>(p,HttpStatus.OK);
+	}
+	
+	@PutMapping("/")
+	public ResponseEntity<?> updateProduct(@RequestBody Product p){
+		p.setName("updateProduct");
+		return new ResponseEntity<>(p,HttpStatus.OK);
 	}
 }
