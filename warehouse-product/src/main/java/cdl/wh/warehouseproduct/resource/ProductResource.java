e01f981e4d333ce3cf070825255893ddfceaee7e
pratik.waghela <pratik.waghela@cdl-it.com>
2019-04-16 20:24:28 +0530

    authorization with gatway

--- warehouse-product/src/main/java/cdl/wh/warehouseproduct/resource/ProductResource.java
+++ warehouse-product/src/main/java/cdl/wh/warehouseproduct/resource/ProductResource.java
@@ -1,15 +1,12 @@
 package cdl.wh.warehouseproduct.resource;
 
-import java.security.Principal;
 import java.util.List;
 
-import org.keycloak.KeycloakSecurityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +14,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import cdl.wh.warehouseproduct.modal.Product;
 import cdl.wh.warehouseproduct.service.ProductService;
