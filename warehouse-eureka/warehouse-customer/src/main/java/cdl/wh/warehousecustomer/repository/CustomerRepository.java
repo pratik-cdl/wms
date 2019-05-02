a571d6fca0a74c1d7e5c190e0e45ebc70ac262ea
pratik.waghela <pratik.waghela@cdl-it.com>
2019-05-02 11:29:48 +0530

    remove microservice from eureka

--- warehouse-eureka/warehouse-customer/src/main/java/cdl/wh/warehousecustomer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package cdl.wh.warehousecustomer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cdl.wh.warehousecustomer.modal.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
